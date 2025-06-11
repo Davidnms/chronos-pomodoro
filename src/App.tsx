39830a05f9fc4e4c16961fb3d2a91bd858fd6f45
David Nicolas <davidnicolas1041@gmail.com>
2025-06-11 11:35:31 -0300

    add: Adicionando parte de Header do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { Heading } from "./components/Heading";
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
 
 import "./styles/theme.css";
 import "./styles/global.css";
 
 export function App() {
-  console.log("Oi");
-
   return (
-    <div>
-      <Heading />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-        recusandae quas aliquid! Itaque soluta dolores ipsum ullam, excepturi
-        modi, sequi quo cum voluptatibus expedita at adipisci nam dicta harum
-        eveniet?
-      </p>
-    </div>
+    <>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu></Menu>
+      </Container>
+    </>
   );
 }
